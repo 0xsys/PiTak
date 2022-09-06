542d3a5e407ad68d808741947152534621c5dfac
Harrison Thompson Buckley <harrisonthompsonbuckley@protonmail.com>
2022-09-06 10:49:53 +0800

    Added function to write a specified number of packets to a .pcap file.

--- classes/sniffer.py
+++ classes/sniffer.py
@@ -2,13 +2,17 @@ from scapy.all import *
 
 class sniffer:
     
-    # Berkley Packet Filter, Time to Live (Number of packets)
-    def __init__(self, bpf, nop):
+    # Berkeley Packet Filter, Packet Count till stop
+    def __init__(self, bpf, pktNum):
         self.filter = bpf
-        self.numberOfPackets = nop
+        self.numberOfPackets = pktNum
 
     def sniffPrint(self, packet):
         packet.show()
 
     def sniffToTerminal(self):
         sniff(filter = self.filter, prn = self.sniffPrint)
+
+    def sniffToFile(self):
+        packets = sniff(filter = self.filter, count = self.numberOfPackets)
+        wrpcap("sPiffer.pcap", packets)
